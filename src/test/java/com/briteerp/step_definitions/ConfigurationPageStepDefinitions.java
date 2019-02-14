6d17cd17a612b6949ba1ef39722a58abc7a4fd3d
Suayip Tekin <suayip.tekin.gmail.com>
2019-02-14 01:05:58 -0600

    Kalyk 1st Test Case complation

--- src/test/java/com/briteerp/step_definitions/ConfigurationPageStepDefinitions.java
+++ src/test/java/com/briteerp/step_definitions/ConfigurationPageStepDefinitions.java
@@ -1,44 +1,42 @@
 package com.briteerp.step_definitions;
 
+import com.briteerp.pages.ConfigurationPage;
 import com.briteerp.pages.LoginPage;
+import com.briteerp.utilities.BrowserUtils;
 import com.briteerp.utilities.ConfigurationReader;
 import com.briteerp.utilities.Driver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ConfigurationPageStepDefinitions {
 
-    @Given("Go to URL")
-    public void go_to_URL() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-    }
-
-    @Then("Click on BriteErpDemo")
-    public void click_on_BriteErpDemo() {
-        LoginPage loginPage = new LoginPage();
-        loginPage.BriteErpDemo.click();
-    }
-
-   /* @Then("Enter valid credentials & click login button")
-    public void enter_valid_credentials_click_login_button() {
-
-    }
-
-    @Then("Navigate and click on Sales module")
-    public void navigate_and_click_on_Sales_module() {
-
-    }
-
     @Then("Navigate to Configuration and click on Sales Channels")
     public void navigate_to_Configuration_and_click_on_Sales_Channels() {
+
+        ConfigurationPage configurationPage= new ConfigurationPage();
+        configurationPage.salesChannel.click();
     }
 
     @Then("Click on Create button for create a new channel")
     public void click_on_Create_button_for_create_a_new_channel() {
+
+        ConfigurationPage configurationPage= new ConfigurationPage();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+        wait.until(ExpectedConditions.urlContains("242"));
+        configurationPage.createNewChannel.click();
+
     }
 
     @Then("Fill up SalesTeamName box and click on Save button")
     public void fill_up_SalesTeamName_box_and_click_on_Save_button() {
+
+        ConfigurationPage configurationPage= new ConfigurationPage();
+        configurationPage.typeName.sendKeys(ConfigurationReader.getProperty("name"));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+        wait.until(ExpectedConditions.urlContains("242"));
+        configurationPage.save.click();
     }
-*/
+
 }
